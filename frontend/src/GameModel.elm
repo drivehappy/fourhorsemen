43429c0c8382ac848a1952daad333a9bda8a880f
drivehappy <drivehappy@gmail.com>
2020-12-06 13:04:50 -0800

    Some basic rendering in Elm now, no network stuff hooked up yet.

---
+++ frontend/src/GameModel.elm
@@ -0,0 +1,45 @@
+module GameModel exposing (..)
+
+
+type alias Vec2 =
+    { x : Float
+    , y : Float
+    }
+
+
+type alias Boss =
+    { position : Vec2
+    , direction : Float   -- Radians
+    , name : String
+    }
+
+type alias EncounterBosses =
+    { mograine : Boss
+    , thane : Boss
+    , zeliek : Boss
+    , blaumeux : Boss
+    }
+
+-- Until we get the server hooked up to send us this, hardcode it
+initBossEncounters =
+    { mograine =
+        { position = { x = 0.5, y = 0.5 }
+        , direction = 0.0
+        , name = "Mograine"
+        }
+    , thane =
+        { position = { x = 0.45, y = 0.5 }
+        , direction = 0.0
+        , name = "Thane"
+        }
+    , zeliek =
+        { position = { x = 0.40, y = 0.5 }
+        , direction = 0.0
+        , name = "Zeliek"
+        }
+    , blaumeux =
+        { position = { x = 0.55, y = 0.5 }
+        , direction = 0.0
+        , name = "Blaumeux"
+        }
+    }
---
+++ frontend/src/GameView.elm
@@ -0,0 +1,112 @@
+module GameView exposing (..)
+
+import Canvas exposing (..)
+import Canvas.Settings exposing (..)
+import Canvas.Settings.Text exposing (..)
+import Color
+import Html exposing (..)
+import Html.Attributes exposing (..)
+
+import GameModel exposing (..)
+
+
+-- View
+
+
+roomWidth = 800
+roomHeight = 800
+
+-- Converts (0-1) dimension into (0-roomWidth (or roomHeight))
+denormalizeX x = roomWidth * x
+denormalizeY y = roomWidth * y
+denormalizePoint x y = (denormalizeX x, denormalizeY y)
+denormalizeVec2 v = denormalizePoint v.x (v.y - 0.02)
+
+
+
+view : Html msg
+view =
+    let
+        clearScreen =
+            shapes [ fill Color.white ] [ rect (0, 0) roomWidth roomHeight ]
+    in
+    Canvas.toHtml
+        (roomWidth, roomHeight)
+        [ style "border" "10px solid rgba(0,0,0,0.6)" ]
+        ( [ clearScreen
+          ]
+          ++ viewRenderPlatform
+          ++ (viewRenderBosses initBossEncounters)
+        )
+
+
+viewRenderPlatform : List Renderable
+viewRenderPlatform =
+    let
+        pillars =
+            shapes [ fill (Color.rgba 0 0 0 0.75) ]
+                [ rect (denormalizePoint 0.3 0.3) (denormalizeX 0.05) (denormalizeY 0.05)
+                , rect (denormalizePoint 0.6 0.3) (denormalizeX 0.05) (denormalizeY 0.05)
+                , rect (denormalizePoint 0.3 0.6) (denormalizeX 0.05) (denormalizeY 0.05)
+                , rect (denormalizePoint 0.6 0.6) (denormalizeX 0.05) (denormalizeY 0.05)
+                ]
+
+        stairs =
+            shapes [ fill (Color.rgba 0 0 0 0.25) ]
+                [ rect (denormalizePoint 0.325 0.325) (denormalizeX 0.3) (denormalizeY 0.025)
+                , rect (denormalizePoint 0.325 0.325) (denormalizeX 0.025) (denormalizeY 0.3)
+                , rect (denormalizePoint 0.6 0.325) (denormalizeX 0.025) (denormalizeY 0.3)
+                , rect (denormalizePoint 0.325 0.6) (denormalizeX 0.3) (denormalizeY 0.025)
+                ]
+    in
+        [ pillars
+        , stairs
+        ]
+
+
+
+renderTextAboveCharacter : Vec2 -> String -> Renderable
+renderTextAboveCharacter charPos text =
+    Canvas.text [ font { size = 12, family = "sans-serif" }, Canvas.Settings.Text.align Center ]
+        (denormalizeVec2 { x = charPos.x, y = charPos.y - 0.02 } )
+        text
+
+
+viewRenderBosses : EncounterBosses -> List Renderable
+viewRenderBosses bosses =
+    let
+        renderBossIndicator : Boss -> Color.Color -> Renderable
+        renderBossIndicator b c =
+            shapes [ fill c ]
+                [ circle (denormalizeVec2 b.position) 15
+                ]
+
+        renderBossNameplate : Boss -> Renderable
+        renderBossNameplate b =
+            renderTextAboveCharacter b.position b.name
+
+    in
+    [ renderBossIndicator bosses.mograine (Color.rgba 1 0 0 1)
+    , renderBossIndicator bosses.thane (Color.rgba 1 0.6 0 1)
+    , renderBossIndicator bosses.zeliek (Color.rgba 0 0 1 1)
+    , renderBossIndicator bosses.blaumeux (Color.rgba 0.3 0.7 0.3 1)
+
+    , renderBossNameplate bosses.mograine
+    , renderBossNameplate bosses.thane
+    , renderBossNameplate bosses.zeliek
+    , renderBossNameplate bosses.blaumeux
+    ]
+
+
+viewRaidTable =
+    table [ style "border" "1px" ]
+        [ tr []
+            [ th [] [ Html.text "Tanks" ]
+            , th [] [ Html.text "Healers" ]
+            , th [] [ Html.text "Melee DPS" ]
+            , th [] [ Html.text "Ranged DPS" ]
+            ]
+        , tr []
+            [ Html.text "Deceit"
+            ]
+        ]
